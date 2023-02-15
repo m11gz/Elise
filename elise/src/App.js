f67cb4e6a1a4739e864ee825793b49ed9a49704c
Miguel Rodriguez <7migro11@gmail.com>
2023-02-14 21:57:54 -0800

    added modal

--- elise/src/App.js
+++ elise/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer/Footer.js';
+import Header from './components/Header/Header.jsx';
+import { Provider, useDispatch } from 'react-redux';
+import store from './store';
+import Menu from './components/Menu/Menu.jsx';
+import SiginForm from './components/SigninForm/SigninForm.jsx';
+import SignupForm from './components/SignupForm/SignupForm.jsx';
+import Hero from './components/Hero/Hero.jsx';
+import Combo from './components/Combo/Combo.jsx';
+import OrderForm from './components/OrderForm/OrderForm.jsx';
+import Cart from './components/Cart/Cart.jsx';
+import Button from 'react-bootstrap/Button';
+
+
 
 function App() {
+  const [cartModalShow, setCartModalShow] = React.useState(false);
+  const [orderModalShow, setOrderModalShow] = React.useState(false);
+  const [signinModalShow, setSigninModalShow] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Header
+        showCart={() => setCartModalShow(true)}
+        showSignin={() => setSigninModalShow(true)}
+      />
+      <Cart show={cartModalShow} onHide={() => setCartModalShow(false)} />
+      <OrderForm
+        show={orderModalShow}
+        onHide={() => setOrderModalShow(false)}
+      />
+      <Hero showOrderForm={() => setOrderModalShow(true)} />
+
+      <Combo />
+      <SiginForm
+        show={signinModalShow}
+        onHide={() => setSigninModalShow(false)}
+      />
+      {/* <SignupForm /> */}
+      <Menu />
+      <Footer />
+    </Provider>
   );
 }
 
